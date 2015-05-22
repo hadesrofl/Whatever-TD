2894961ffe7400de220dd3cd5cc6ffe906a64685
birdib0y <florianwinzek@web.de>
2015-05-22 13:55:10 +0200

    fixi foxi

--- TowerDefense/lib/model/tower/tower.dart
+++ TowerDefense/lib/model/tower/tower.dart
@@ -1,5 +1,4 @@
 part of tower;
-import 'package:tower/damage.dart' as Damage;
 class Tower{
   int x;
   int y;
@@ -13,7 +12,68 @@ class Tower{
   Damage damage;
   
   Tower(){
+    target = new Target();
+    target.iwas();
+  }
+  Target shoot(Minion minion){
     
+  }
+  void sell(){
+    
+  }
+  void setCoordinates(){
+    
+  }
+  void upgrade(){
+    
+  }
+  bool abilityCalculation(){
+    return null;
+  }
+  
+  // --------------getter-/setter methods-----------------//
+  
+  int getX(){
+    return this.x;
+  }
+  void setX(int x){
+    this.x = x;
+  }
+  int getY(){
+    return this.y;
+  }
+  void setY(){
+    this.y = y;
+  }
+  int getAttackField(){
+    return this.attackField;
+  }
+  void setAttackField(int attackField){
+    this.attackField = attackField;
+  }
+  int getPrice(){
+    return this.price;
+  }
+  void setPrice(int price){
+    this.price = price;
+  }
+  int getSellingPrice(){
+    return this.sellingPrice;
+  }
+  void setSellingPrice(int p){
+    this.sellingPrice = p;
+  }
+  int getUpgradeLevel(){
+    return this.upgradeLevel;
+  }
+  void setUpgradeLevel(int ul){
+    this.upgradeLevel = ul;
+  }
+  double getAttackSpeed(){
+    return this.attackSpeed;
+  }
+  void setAttackSpeed(double as){
+    this.attackSpeed = as;
   }
   
 }
