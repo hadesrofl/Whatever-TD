53e87351d8a7c27130099660295996e67a79ff85
birdib0y <florianwinzek@web.de>
2015-05-22 11:31:35 +0200

    Klasse Game Struktur

---
+++ TowerDefense/lib/model/game.dart
@@ -0,0 +1,25 @@
+class Game{
+  Map<int, int> board;
+  
+  Game(){
+    board = new Map<int, int>();
+  }
+  void startGame(){
+    
+  }
+  void runGame(){
+    
+  }
+  void endOfGame(){
+    
+  }
+  void updateView(){
+    
+  }
+  void endOfLevel(){
+    
+  }
+  void calculateHighScore(){
+    
+  }
+}
