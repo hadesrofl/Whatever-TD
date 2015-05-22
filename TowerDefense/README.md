b083624eff5292b86cdaf0980487ea09fa4ae755
birdib0y <florianwinzek@web.de>
2015-05-22 11:19:28 +0200

    Klassendiagramm fertig

---
+++ TowerDefense/README.md
@@ -0,0 +1,3 @@
+# TowerDefense
+
+A sample command-line application.
---
+++ TowerDefense/bin/main.dart
@@ -0,0 +1,8 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:TowerDefense/TowerDefense.dart' as TowerDefense;
+
+main(List<String> arguments) {
+  print('Hello world: ${TowerDefense.calculate()}!');
+}
