6673ba0b43376ec28f10781a4d386d2ae4163e50
Rene <renejon@web.de>
2015-06-08 08:35:20 +0200

    Fixed Merge

--- TowerDefense/lib/model/game.dart
+++ TowerDefense/lib/model/game.dart
@@ -1,73 +1,3 @@
-<<<<<<< HEAD
-import "tower/towerAdmin.dart";
-import "level/levelAdmin.dart";
-import "field.dart";
-import "dart:io";
-import "../view/view.dart";
-import "player.dart";
-
-class game {
-  TowerAdmin tAdmin;
-  LevelAdmin lAdmin;
-  File levels;
-  Map<String, Field> board;
-  Map<int, String> images;
-  final row = 22;
-  final col = 22;
-  View view;
-  Player player;
-
-  game() {
-    //TODO: Enter concrete FilePath
-    this.levels = new File(Platform.script.toFilePath());
-    this.tAdmin = new TowerAdmin();
-    this.lAdmin = new LevelAdmin(levels);
-    this.board = new Map<String, Field>();
-  }
-  /**
-   * 
-   */
-  void startGame() {
-    this.board = lAdmin.createBoard(this.row, this.col);
-    this.view = new View(convertBoard());
-    this.player = new Player(this.view.getPlayerName().toString());
-  }
-  /**
-   * 
-   */
-  void runGame() {}
-  /**
-   * 
-   */
-  void endOfGame() {}
-  /**
-   * 
-   */
-  void updateView() {}
-  /**
-   * 
-   */
-  void endOfLevel() {}
-  /**
-   * 
-   */
-  void calculateHighScore() {}
-  /**
-   * converts the Map from <String, Field> to <String, String> to create a HTML-Table
-   */
-  Map<String, String> convertBoard() {
-    Map<String, String> htmlBoard = new Map<String, String>();
-    String keySet;
-    var str;
-    this.board.forEach((key, value) {
-      keySet = value.getX().toString() + " " + value.getY().toString();
-      str = keySet.split(" ");
-      htmlBoard.putIfAbsent(str[0], () => str[1]);
-    });
-    return htmlBoard;
-  }
-}
-=======
 import "tower/towerAdmin.dart";
 import "level/levelAdmin.dart";
 import "field.dart";
@@ -142,4 +72,3 @@ class Game {
     this.view = view;
   }
 }
->>>>>>> 64f68d08116f5dc4adfa56fbcf14d70aa23a9455
--- TowerDefense/lib/view/view.dart
+++ TowerDefense/lib/view/view.dart
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-library view;
-
-import "dart:html";
-
-/**
-   * This Class handles the appearance of our game and manipulates the DOM-Tree of HTML
-   */
-class View {
-/**
-   * An inputElement so that the user can type in his name
-   */
-  InputElement nameInput;
-/**
-   * A start button to start or continue the game
-   */
-  ButtonElement start;
-/**
-   * A stop button if the player wants to stop the game
-   */
-  ButtonElement stop;
-/**
-   * The board as table. The board has a given size
-   */
-  TableElement board;
-
-/**
-   * Constructor of the view
-   * @param board - this is the datastructure for our board in dart. 
-   * It cointains informations about the objects on the board which 
-   * will be displayed in the view as images
-   */
-  View(Map<String, String> board) {
-    nameInput = querySelector("#playerName");
-    start = querySelector("#start");
-    stop = querySelector("#stop");
-    this.board = createHTMLTable(board);
-  }
-/**
-   * Method to create a board 
-   * @param board - contains all information which shall be displayed in the view
-   * @return is a table element for the DOM-Tree
-   */
-  TableElement createHTMLTable(Map<String, String> board) {
-//TODO: Do fancy creating stuff
-    return null;
-  }
-/**
-   * Method to update a board 
-   * @param board - contains all current information which shall be displayed in the view
-   */
-  void updateBoard() {
-//TODO: Do fancy updating stuff
-  }
-/**
-   * Method to update the page
-   */
-  void updatePage() {
-//TODO: Do fancy updating stuff _ rev 2
-  }
-
-  //-------------------getter-/setterMethods---------------------------//
-
-  InputElement getPlayerName() {
-    return this.nameInput;
-  }
-}
-=======
 library view;
 
 import "dart:html";
@@ -158,4 +90,3 @@ class View {
   }
 
 }
->>>>>>> 64f68d08116f5dc4adfa56fbcf14d70aa23a9455
